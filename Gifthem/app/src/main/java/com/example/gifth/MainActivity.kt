9346e039a04b2d461d534fc19ecf5099541ba5c6
Andrés Marín <amarinb1@alumnos.nebrija.es>
2023-03-01 13:04:19 +0100

    update 0.22

--- Gifthem/app/src/main/java/com/example/gifth/MainActivity.kt
+++ Gifthem/app/src/main/java/com/example/gifth/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.gifth
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +14,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gifth.ui.theme.GifthTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
+    private var customToken: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
         setContent {
             GifthTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +44,43 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+    private fun startSignIn() {
+        // Initiate sign in with custom token
+        // [START sign_in_custom]
+        customToken?.let {
+            auth.signInWithCustomToken(it)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCustomToken:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+        // [END sign_in_custom]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    companion object {
+        private const val TAG = "CustomAuthActivity"
     }
 }
 
