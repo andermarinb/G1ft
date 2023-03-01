8371b5400ff4985369ecf73a04124d2f391fa063
Andrés Marín <amarinb1@alumnos.nebrija.es>
2023-03-01 12:20:40 +0100

    Update 0.2

---
+++ Gifthem/app/src/main/java/com/example/gifth/model/User.kt
@@ -0,0 +1,7 @@
+package com.example.gifth.model
+
+data class User (
+    val id: String,
+    val name: String,
+    val bdate: String
+)
--- Gifthem/app/src/main/java/com/example/gifth/views/GiftViewModel.kt
+++ Gifthem/app/src/main/java/com/example/gifth/views/GiftViewModel.kt
@@ -3,9 +3,10 @@ package com.example.gifth.views
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.example.gifth.model.Gift
+import com.google.firebase.firestore.FirebaseFirestore
 
 class GiftViewModel: ViewModel() {
-    private val db = Firebase.firestore
+    private val db = FirebaseFirestore.getInstance()
     private val giftsCollection = db.collection("gifts")
     val gifts = mutableStateListOf<Gift>()
 
