dcbb5522bcadff7ef208c9bc39413130c8238657
Andrés Marín <amarinb1@alumnos.nebrija.es>
2023-03-01 12:32:14 +0100

    Update 0.21

--- Gifthem/app/src/main/java/com/example/gifth/views/UserViewModel.kt
+++ Gifthem/app/src/main/java/com/example/gifth/views/UserViewModel.kt
@@ -1,26 +1,26 @@
 package com.example.gifth.views
 
 import androidx.compose.runtime.mutableStateListOf
-import androidx.lifecycle.ViewModel
 import com.example.gifth.model.Gift
+import com.example.gifth.model.User
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 
-class GiftViewModel: ViewModel() {
+class UserViewModel {
     private val db = FirebaseFirestore.getInstance()
     private val giftsCollection = db.collection("gifts")
-    val gifts = mutableStateListOf<Gift>()
+    val users = mutableStateListOf<User>()
 
-    fun addG(gift:Gift){
-        giftsCollection.document(gift.id).set(gift)
+    fun addG(user: User){
+        giftsCollection.document(user.id).set(user)
     }
 
     fun getGifts(){
         giftsCollection.get().addOnSuccessListener{ result ->
-            gifts.clear()
+            users.clear()
             for(document in result){
-                val gift = document.toObject<Gift>()
-                gifts.add(gift)
+                val gift = document.toObject<User>()
+                users.add(gift)
             }
         }
     }
