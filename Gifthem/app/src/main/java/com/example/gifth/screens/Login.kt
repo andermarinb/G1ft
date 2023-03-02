626db263908dab5a09baa0e1be02c394775baabd
Andrés Marín <amarinb1@alumnos.nebrija.es>
2023-03-02 10:57:03 +0100

    Update 0.24

---
+++ Gifthem/app/src/main/java/com/example/gifth/screens/Login.kt
@@ -0,0 +1,123 @@
+package com.example.gifth.screens
+
+import android.annotation.SuppressLint
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import com.example.gifth.R
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.gifth.MainActivity
+import com.example.gifth.TopBar
+import com.example.gifth.views.LoginViewModel
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun Login(navController: NavController) {
+    navController.enableOnBackPressed(false)
+    val activity = LocalContext.current as MainActivity
+    val viewModel: LoginViewModel by activity.viewModels()
+    val loggedUser by viewModel.loggedUser().observeAsState(null)
+    val logged by viewModel.logged().observeAsState(false)
+    if (loggedUser != null && !logged) {
+        loggedUser!!.displayName?.let {
+            PopUpLogin(it) {
+                viewModel.logIn()
+                navController.navigate("main_menu")
+
+            }
+        }
+    }
+    Scaffold(
+        topBar = { TopBar(pageName = "EyPuta", navController = navController, backbutton = false) },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Image(modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.FillWidth,painter = painterResource(id =
+                R.drawable.gifticon), contentDescription = "Banner")
+            LoginForm(navController, viewModel, activity)
+        }
+    }
+}
+
+
+@Composable
+fun LoginForm(navController: NavController, viewModel: LoginViewModel, activity: MainActivity) {
+    val isLoading by viewModel.isLoading().observeAsState(false)
+    val googleError by viewModel.googleError().observeAsState("")
+    val hasGoogleError by viewModel.hasGoogleError().observeAsState(false)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp)
+    ) {
+
+        ButtonAccces(text = "Acceder con Google", icon = R.drawable.ic_launcher_foreground) {
+            viewModel.logInWithGoogle(activity)
+        }
+
+        // ERROR TEXT
+        if (hasGoogleError){
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(text = googleError, color = Color.Red)
+        }
+
+        if (isLoading){
+            CircularProgressIndicator()
+        }
+    }
+}
+
+
+@Composable
+fun ButtonAccces(text: String, icon: Int?, onClick: () -> Unit) {
+
+    Button(
+        onClick = onClick, modifier = Modifier.width(350.dp)
+    ) {
+        if (icon != null){
+            Icon(painter =  painterResource(id = icon), contentDescription = "")
+            Spacer(modifier = Modifier.size(10.dp))
+        }
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.button
+        )
+    }
+}
+
+@Composable
+fun PopUpLogin(name: String, onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onSurface
+            )
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = "Continuar")
+            }
+        },
+        backgroundColor = MaterialTheme.colors.surface
+    )
+}
